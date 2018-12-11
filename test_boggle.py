be5724764e1ac96e95bf19d6e715a1dc5e1a1cde
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-11 15:34:11 +0000

    Added useful code for understanding

--- test_boggle.py
+++ test_boggle.py
@@ -3,7 +3,7 @@ import unittest
 
 #Set a class called boggle_test then call a function with self parameter for testing
 #Self represents or points the instance which it was called, self is the first thing that
-#is passed to a function in a class, the object itself is passes as an argument
+#is passed to a function in a class, the object itself is passed as an argument
 class boggle_test(unittest.TestCase):
 #Calling the function for testing if two values are equal
     def test_is_this_thing_on(self):
