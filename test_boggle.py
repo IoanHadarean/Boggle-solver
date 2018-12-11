193e10ae4791c9c4e0df2305869155ab7fc69ba3
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-11 15:16:39 +0000

    Added boggle.py and test_boggle.py/added useful code

---
+++ test_boggle.py
@@ -0,0 +1,10 @@
+#Importing the module unittest
+import unittest
+
+#Set a class called boggle_test then call a function with self parameter for testing
+#Self represents or points the instance which it was called
+class boggle_test(unittest.TestCase):
+#Calling the function for testing if two values are equal
+    def test_is_this_thing_on(self):
+        self.assertEqual(1, 1)
+        
