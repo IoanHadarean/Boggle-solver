43c0209a1359409e665bb82d84c17ee5e734108c
Ioan Hadarean <hadareannelutu@yahoo.com>
2018-12-12 13:24:25 +0000

    Added test and function for neighbours of position

--- test_boggle.py
+++ test_boggle.py
@@ -53,6 +53,21 @@ class TestBoggle(unittest.TestCase):
         """
         Ensure that each of the coordinates in the grid contains letters
         """
-        grid = boggle.make_grid(2,2)
+        grid = boggle.make_grid(2,3)
         for letter in grid.values():
             self.assertIn(letter, ascii_uppercase)
+            
+    def test_neighbours_of_a_position(self):
+        """
+        Ensure that a position has 8 neighbours
+        """
+        coords = (1,2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0,1), neighbours)
+        self.assertIn((0,2), neighbours)
+        self.assertIn((0,3), neighbours)
+        self.assertIn((1,1), neighbours)
+        self.assertIn((1,3), neighbours)
+        self.assertIn((2,1), neighbours)
+        self.assertIn((2,2), neighbours)
+        self.assertIn((2,3), neighbours)
